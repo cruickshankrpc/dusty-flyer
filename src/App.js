51aa22e0878bcbeccece728c3b615cc41f5abf77
Raquel <cruickshankrpc@gmail.com>
2024-10-01 11:26:45 +0100

    dl tailwind, scss, bg img, custom font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
-
+import "./App.scss";
+import bgImg from "./bg-img.jpg";
 function App() {
   return (
     <div className="App">
-      <h1>Dusty Flyer</h1>
+      <div className="relative min-h-screen">
+        <img
+          src={bgImg}
+          className="absolute inset-0 w-full h-full object-cover opacity-80"
+        />
+        <div id="register" className="w-full max-w-md mx-auto text-center">
+          <h2 className="font-broadsheet text-4xl">Register for updates</h2>
+        </div>
+      </div>
     </div>
   );
 }
