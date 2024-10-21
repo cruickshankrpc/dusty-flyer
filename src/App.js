1c0867b5b566bea5bb841bfb508ddfc7112bfa84
Raquel <cruickshankrpc@gmail.com>
2024-10-21 22:59:32 +0900

    move pic to images folder in src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import bgImg from "./bg-img.jpg";
+import bgPic from "./images/bg-pic.jpg";
 import { useState } from "react";
 import { PhoneInput } from "react-international-phone";
 import "react-international-phone/style.css";
@@ -10,27 +11,29 @@ function App() {
   return (
     <div className="App">
       <div className="relative min-h-screen">
-        <img
-          src={bgImg}
-          className="absolute inset-0 w-full h-full object-cover opacity-80"
-        />
-        <div
-          id="register"
-          className="font-broadsheet text-black w-full max-w-md mx-auto py-20 text-center relative"
-        >
-          <h2 className="text-4xl">Register for updates</h2>
+        // COPYCAT
+        <div>
+          <img
+            src={bgImg}
+            className="absolute inset-0 w-full h-full object-cover opacity-80"
+          />
+          <div
+            id="register"
+            className="font-broadsheet text-black w-full max-w-md mx-auto py-20 text-center relative"
+          >
+            <h2 className="text-4xl">Register for updates</h2>
 
-          <div className="flex flex-col space-y-2">
-            <form className="flex flex-col space-y-2 items-stretch">
-              <input
-                type="email"
-                placeholder="Email*"
-                autocomplete="email"
-                className="input"
-              />
-              <input type="number" placeholder="Phone*" className="input" />
-              <button class="register-button">Register</button>
-              {/* <div className="flex items-stretch space-x-2 flex-grow">
+            <div className="flex flex-col space-y-2">
+              <form className="flex flex-col space-y-2 items-stretch">
+                <input
+                  type="email"
+                  placeholder="Email*"
+                  autocomplete="email"
+                  className="input"
+                />
+                <input type="number" placeholder="Phone*" className="input" />
+                <button class="register-button">Register</button>
+                {/* <div className="flex items-stretch space-x-2 flex-grow">
                 <PhoneInput
                   className="input"
                   defaultCountry="gb"
@@ -38,12 +41,26 @@ function App() {
                   onChange={(phone) => setPhone(phone)}
                 />
               </div> */}
-              <p className="text-small my-4 leading-snug">
-                By signing up you are consenting to receive emails from
-                Frequency Wizards Ltd. We will keep you up to date with any
-                relevant events. You can unsubscribe at any time.
-              </p>
-            </form>
+                <p className="text-small my-4 leading-snug">
+                  By signing up you are consenting to receive emails from
+                  Frequency Wizards Ltd. We will keep you up to date with any
+                  relevant events. You can unsubscribe at any time.
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+        // DFC
+        <div>
+          <div>
+            <img
+              src={bgPic}
+              className="absolute inset-0 w-full h-full object-cover opacity-80"
+            />
+            <div
+              id="register"
+              className="font-broadsheet text-black w-full max-w-md mx-auto py-20 text-center relative"
+            ></div>
           </div>
         </div>
       </div>
