6dd425fb8e872ed6c1ae79c05f50c5852ce5ce04
Raquel <cruickshankrpc@gmail.com>
2024-10-29 21:22:19 +0900

    add favicon & title, use scss, add social media w links

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
 import "./App.scss";
-import bgImg from "./bg-img.jpg";
 import bgPic from "./images/bg-pic.jpg";
 import logo from "./images/DFC-logo.png";
-import { useState } from "react";
-import { PhoneInput } from "react-international-phone";
-import "react-international-phone/style.css";
+import instagramIcon from "./images/instagram.png";
+import emailIcon from "./images/email.png";
+import facebookIcon from "./images/facebook.png";
 
 function App() {
-  const [phone, setPhone] = useState("");
-
   return (
     <div className="App">
-      <div className="relative min-h-screen">
-        {/* COPYCAT */}
-        {/* <div>
-          <img
-            src={bgImg}
-            className="absolute inset-0 w-full h-full object-cover opacity-80"
-          />
-          <div
-            id="register"
-            className="font-broadsheet text-black w-full max-w-md mx-auto py-20 text-center relative"
-          >
-            <h2 className="text-4xl">Register for updates</h2>
-
-            <div className="flex flex-col space-y-2">
-              <form className="flex flex-col space-y-2 items-stretch">
-                <input
-                  type="email"
-                  placeholder="Email*"
-                  autocomplete="email"
-                  className="input"
-                />
-                <input type="number" placeholder="Phone*" className="input" />
-                <button class="register-button">Register</button>
-                <p className="text-small my-4 leading-snug">
-                  By signing up you are consenting to receive emails from
-                  Frequency Wizards Ltd. We will keep you up to date with any
-                  relevant events. You can unsubscribe at any time.
-                </p>
-              </form>
-            </div>
-          </div>
-        </div> */}
-        {/* DFC */}
-        <div>
-          <img
-            src={bgPic}
-            className="absolute inset-0 w-full h-full object-cover opacity-80"
-          />
-          <div className="flex justify-center">
-            <img className="max-w-xs absolute" src={logo}></img>
+      <div className="container">
+        <img src={bgPic} alt="bg" className="c-bg-img" />
+        <div className="content">
+          <img className="logo" alt="Dusty Flyers' Logo" src={logo}></img>
+          <div className="icon-container">
+            <a
+              href="https://www.instagram.com/dustyflyersclub/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="icon" src={instagramIcon} alt="Instagram Icon" />
+            </a>
+            <a
+              href="https://www.facebook.com/dustyflyersclub/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="icon" src={facebookIcon} alt="Facebook Icon" />
+            </a>
+            <a
+              href="mailto:info@dustyflyersclub.club"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="email-icon" src={emailIcon} alt="Email Icon" />
+            </a>
           </div>
         </div>
       </div>
